53488c2d952dd5b533bc5920cd75e31c0b847047
Louisk3 <156825888+Louisk3@users.noreply.github.com>
2024-07-24 10:42:28 +0100

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import inject from '@rollup/plugin-inject';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import resolve from '@rollup/plugin-node-resolve';
-import inject from '@rollup/plugin-inject'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+
   return {
     base: "./",
-    define:
-      mode === 'development' ?
-        {
-          'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY),
-          'global': {}
-        } : {
-          'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY),
-        },
-    build:
-      mode === 'production'
-        ? {
-          rollupOptions: {
-            plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-          },
-        }
-        : undefined,
-
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY),
+      'global': 'globalThis',
+    },
+    build: {
+      rollupOptions: {
+        plugins: [
+          inject({
+            Buffer: ['buffer', 'Buffer'],
+            process: 'process/browser'
+          }),
+          nodePolyfills()
+        ]
+      }
+    },
     optimizeDeps: {
       include: ['@emotion/styled'],
       esbuildOptions: {
@@ -36,23 +36,31 @@ export default defineConfig(({ mode }) => {
         // Enable esbuild polyfill plugins
         plugins: [
           NodeGlobalsPolyfillPlugin({
-            buffer: true
-          })
+            buffer: true,
+            process: true
+          }),
+          NodeModulesPolyfillPlugin()
         ]
       }
     },
     resolve: {
       alias: {
-        crypto: 'crypto-browserify'
+        crypto: 'crypto-browserify',
+        stream: 'stream-browserify',
+        assert: 'assert',
+        http: 'stream-http',
+        https: 'https-browserify',
+        os: 'os-browserify/browser',
+        url: 'url',
+        process: 'process'
       }
     },
-    plugins:
-      [
-        react(),
-        resolve({
-          browser: true,
-          preferBuiltins: false,
-        })
-      ],
-  }
-})
+    plugins: [
+      react(),
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      })
+    ]
+  };
+});
